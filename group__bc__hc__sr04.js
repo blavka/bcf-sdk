82f9c4a6b8c94473232342d29eb122a7f1259c7e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-23 07:28:29 +0000

    Deploy sdk.hardwario.com to github.com/hardwario/bcf-sdk.git:gh-pages

---
+++ group__bc__hc__sr04.js
@@ -0,0 +1,14 @@
+var group__bc__hc__sr04 =
+[
+    [ "bc_hc_sr04_t", "group__bc__hc__sr04.html#ga5b4a1fe44f875b6e8c09c111c18f86c8", null ],
+    [ "bc_hc_sr04_event_t", "group__bc__hc__sr04.html#ga851a8825f946d0fe31b1a0641012311c", [
+      [ "BC_HC_SR04_EVENT_ERROR", "group__bc__hc__sr04.html#gga851a8825f946d0fe31b1a0641012311ca71cf7b959794c25b37065c937befeebf", null ],
+      [ "BC_HC_SR04_EVENT_UPDATE", "group__bc__hc__sr04.html#gga851a8825f946d0fe31b1a0641012311ca540b170252bf9cb0096fe2a6359d8609", null ]
+    ] ],
+    [ "bc_hc_sr04_get_distance_millimeter", "group__bc__hc__sr04.html#ga3719fc6818d0a5ff4986119d0342f91e", null ],
+    [ "bc_hc_sr04_init", "group__bc__hc__sr04.html#gadd8119b600256a956943b1064ce46988", null ],
+    [ "bc_hc_sr04_init_sensor_module", "group__bc__hc__sr04.html#ga7b3957cd82938fbd01934b413a71f5b0", null ],
+    [ "bc_hc_sr04_measure", "group__bc__hc__sr04.html#ga604541fdde96919d5431c97b8b258f1b", null ],
+    [ "bc_hc_sr04_set_event_handler", "group__bc__hc__sr04.html#ga7ec82bbc8585aa3acfe6f0bd66b243c8", null ],
+    [ "bc_hc_sr04_set_update_interval", "group__bc__hc__sr04.html#ga75e891bf7bca30de4b5b1df67fceb6e7", null ]
+];
